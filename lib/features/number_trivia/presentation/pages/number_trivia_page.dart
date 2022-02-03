8ffdc419ac2d469b28dbe32dc3a6533c6b34fd51
fauzanafism <fauzan137@gmail.com>
2022-02-04 05:47:45 +0700

    added skeleton UI

---
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NumberTriviaPage extends StatelessWidget {
+  const NumberTriviaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Number Trivia'),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(),
+    );
+  }
+}
