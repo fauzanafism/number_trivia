05531baaeecc3c1cab9d8cf6991ab30b3c02694d
fauzanafism <fauzan137@gmail.com>
2022-02-04 17:48:59 +0700

    changed int to int?

--- test/features/number_trivia/domain/usecases/get_concrete_nimber_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_nimber_test.dart
@@ -1,5 +1,7 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number.dart';
 
@@ -8,10 +10,17 @@ class MockNumberTriviaRepository extends Mock
 
 void main() {
   GetConcreteNumberTrivia usecase;
-  MockNumberTriviaRepository mockNumberTriviaRepository;
+  MockNumberTriviaRepository mockNumberTriviaRepository = MockNumberTriviaRepository();
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
     usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  final tNumber = 1;
+  final tNumberTrivia = NumberTrivia(text: 'test', number: 1);
+
+  test('should get trivia for the number from the repository', () async {
+    when(mockNumberTriviaRepository.getConcreteNumberTrivia(any)).thenAnswer((_) async => Right(tNumberTrivia));
   });
 }
