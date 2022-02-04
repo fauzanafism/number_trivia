f314e81868992d1722e4a8ae4c967fd3173d6b33
fauzanafism <fauzan137@gmail.com>
2022-02-04 17:39:04 +0700

    added usecases

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number.dart
@@ -0,0 +1,6 @@
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+  GetConcreteNumberTrivia(this.repository);
+}
