72ff5b0d62d16da34ddc999d0f7be410deb80777
fauzanafism <fauzan137@gmail.com>
2022-02-06 06:25:01 +0700

    generated mock class

---
+++ test/core/test_helpers.dart
@@ -0,0 +1,9 @@
+import 'package:mockito/annotations.dart';
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+@GenerateMocks(
+  [
+    NumberTriviaRepository
+  ],
+)
+void main() {}
