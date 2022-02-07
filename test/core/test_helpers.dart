45fe6221533541086ee05726405881d78b44119d
fauzanafism <fauzan137@gmail.com>
2022-02-07 13:54:57 +0700

    added 3 mock classes

--- test/core/test_helpers.dart
+++ test/core/test_helpers.dart
@@ -1,9 +1,15 @@
 import 'package:mockito/annotations.dart';
+import 'package:number_trivia/core/platform/network_info.dart';
+import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
 @GenerateMocks(
   [
-    NumberTriviaRepository
+    NumberTriviaRepository,
+    NumberTriviaRemoteDataSource,
+    NumberTriviaLocalDataSource,
+    NetworkInfo,
   ],
 )
 void main() {}
