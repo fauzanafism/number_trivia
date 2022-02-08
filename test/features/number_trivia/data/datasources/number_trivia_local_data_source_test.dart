e18911b38832c9f17a331ed41e520f6625589608
fauzanafism <fauzan137@gmail.com>
2022-02-08 17:31:16 +0700

    added test for when there is no cached data

--- test/features/number_trivia/data/datasources/number_trivia_local_data_source_test.dart
+++ test/features/number_trivia/data/datasources/number_trivia_local_data_source_test.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/error/exception.dart';
 import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
 
@@ -30,6 +31,17 @@ void main() {
       // assert
       verify(mockSharedPreferences.getString('CACHED_NUMBER_TRIVIA'));
       expect(result, equals(tNumberTriviaModel));
+    });
+
+    test('should throw a CacheException when there is not a cached value', () {
+      // arrange
+      when(mockSharedPreferences.getString(any)).thenReturn(null);
+      // act
+      // ga dipanggil disini
+      final call = dataSource.getLastNumberTrivia;
+      // assert
+      // perlu untuk tes jika panggil metod yg throw exception
+      expect(() =>  call(), throwsA(const TypeMatcher<CacheException>()));
     });
   });
 }
