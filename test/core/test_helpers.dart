2b349b8698eb8dcf52d42f7e24d8bfb9c96c062a
fauzanafism <fauzan137@gmail.com>
2022-02-08 17:09:46 +0700

    added sharedpref mock

--- test/core/test_helpers.dart
+++ test/core/test_helpers.dart
@@ -1,8 +1,10 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:mockito/annotations.dart';
 import 'package:number_trivia/core/network/network_info.dart';
 import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 @GenerateMocks(
   [
@@ -10,6 +12,8 @@ import 'package:number_trivia/features/number_trivia/domain/repositories/number_
     NumberTriviaRemoteDataSource,
     NumberTriviaLocalDataSource,
     NetworkInfo,
+    InternetConnectionChecker,
+    SharedPreferences
   ],
 )
 void main() {}
