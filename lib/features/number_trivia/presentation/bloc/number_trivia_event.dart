60488fb0c4bc114d491ea60b9560adbf6bc48a40
fauzanafism <fauzan137@gmail.com>
2022-02-10 06:25:43 +0700

    added ConcreteNumber and RandomNumber events

--- lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -1,8 +1,20 @@
 part of 'number_trivia_bloc.dart';
 
-abstract class NumberTriviaEvent extends Equatable {
+abstract class NumberTriviaEvent extends Equatable { 
   const NumberTriviaEvent();
 
   @override
   List<Object> get props => [];
 }
+
+// ignore: empty_constructor_bodies
+class GetTriviaForConcreteNumber extends NumberTriviaEvent {
+  final String numberString;
+
+  GetTriviaForConcreteNumber(this.numberString)
+
+  @override
+  List<Object> get props => [numberString]; 
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaEvent {}
