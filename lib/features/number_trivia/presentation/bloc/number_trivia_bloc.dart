b38468f54b2edc040bfdbb2125cf086b15c2ea65
fauzanafism <fauzan137@gmail.com>
2022-02-10 22:43:52 +0700

    added constant strings

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -14,6 +14,11 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   final GetRandomNumberTrivia getRandomNumberTrivia;
   final InputConverter inputConverter;
 
+  static const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+  static const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+  static const String INVALID_INPUT_FAILURE_MESSAGE =
+      'Invalid Input - The number must be a positive integer or zero.';
+
   NumberTriviaBloc(
       {required this.getConcreteNumberTrivia,
       required this.getRandomNumberTrivia,
