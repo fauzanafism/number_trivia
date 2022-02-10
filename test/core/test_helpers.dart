4d534904d2a4db7adaa50ebfd7a59d119fd55a5f
fauzanafism <fauzan137@gmail.com>
2022-02-10 20:17:48 +0700

    added mock InputConverter and two usecase classes

--- test/core/test_helpers.dart
+++ test/core/test_helpers.dart
@@ -1,9 +1,12 @@
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:mockito/annotations.dart';
 import 'package:number_trivia/core/network/network_info.dart';
+import 'package:number_trivia/core/util/input_converter.dart';
 import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_number.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
@@ -15,7 +18,10 @@ import 'package:http/http.dart' as http;
     NetworkInfo,
     InternetConnectionChecker,
     SharedPreferences,
-    http.Client
+    http.Client,
+    InputConverter,
+    GetConcreteNumberTrivia,
+    GetRandomNumberTrivia,
   ],
 )
 void main() {}
