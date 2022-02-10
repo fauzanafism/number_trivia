bf1524a6128990b8a5272cc57e542f82ebc0e337
fauzanafism <fauzan137@gmail.com>
2022-02-10 22:35:14 +0700

    added test for when converting string to int

--- test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
+++ test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
@@ -1,4 +1,7 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 
 import '../../../../core/test_helpers.mocks.dart';
@@ -18,5 +21,29 @@ void main() {
         getConcreteNumberTrivia: mockGetConcreteNumberTrivia,
         getRandomNumberTrivia: mockGetRandomNumberTrivia,
         inputConverter: mockInputConverter);
+  });
+
+  test('initialState should be Empty', () {
+    // assert
+    expect(bloc.state, equals(Empty()));
+  });
+
+  group('GetTriviaForConcreteNumber', () {
+    // Event ngambil data string
+    const tNumberString = '1';
+    // Ini yg udah diparse
+    final tNumberParsed = int.parse(tNumberString);
+    // ini entity
+    const tNumberTrivia = NumberTrivia(text: 'test trivia', number: 1);
+
+    test('should call the InputConverter to validate and convert the string to an unsigned int', () async {
+      //  arrange
+      when(mockInputConverter.stringToUnsignedInt(any)).thenReturn(Right(tNumberParsed));
+      // act
+      bloc.add(const GetTriviaForConcreteNumber(tNumberString));
+      await untilCalled(mockInputConverter.stringToUnsignedInt(any));
+      // assert
+      verify(mockInputConverter.stringToUnsignedInt(tNumberString));
+    });
   });
 }
