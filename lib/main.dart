edfdefb78501fb92717a97f818a37d8861b15edb
fauzanafism <fauzan137@gmail.com>
2022-02-11 17:24:40 +0700

    added injection init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:number_trivia/features/number_trivia/presentation/pages/number_trivia_page.dart';
+import 'injection_container.dart' as di;
 
-void main() {
+void main() async {
+  await di.init();
   runApp(const MyApp());
 }
 
