21ac28131ca982427c5e353b7c07d9b77effc8c5
fauzanafism <fauzan137@gmail.com>
2022-02-12 02:25:22 +0700

    added bloc builder and completed it

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 import 'package:number_trivia/features/number_trivia/presentation/widgets/widgets.dart';
+import 'package:number_trivia/injection_container.dart';
 
 class NumberTriviaPage extends StatelessWidget {
   const NumberTriviaPage({Key? key}) : super(key: key);
@@ -11,18 +14,45 @@ class NumberTriviaPage extends StatelessWidget {
         title: const Text('Number Trivia'),
         backgroundColor: Colors.amber,
       ),
-      body: Center(
+      body: SingleChildScrollView(child: buildBody(context)),
+    );
+  }
+
+  BlocProvider<NumberTriviaBloc> buildBody(BuildContext context) {
+    return BlocProvider(
+      create: (_) => sl<NumberTriviaBloc>(),
+      child: Center(
         child: Padding(
           padding: const EdgeInsets.all(10),
-          child: Column(children: const <Widget>[
-            SizedBox(height: 10,),
-            MessageDisplay(message: 'Start searching!',),
-            SizedBox(height: 20,),
-            TriviaControls(),
-          ],),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 10,
+              ),
+              BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+                builder: (context, state) {
+                  if (state is Empty) {
+                    return const MessageDisplay(
+                      message: 'Start searching!',
+                    );
+                  } else if (state is Loading) {
+                    return const LoadingWidget();
+                  } else if (state is Loaded) {
+                    return TriviaDisplay(numberTrivia: state.trivia,);
+                  } else if (state is Error) {
+                    return MessageDisplay(message: state.message);
+
+                  }
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const TriviaControls(),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
